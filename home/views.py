9ba9146fc8e045797bc5e9f51179a6a87ac47ccd
cankayaci <can.kayaci01@gmail.com>
2022-04-26 02:08:19 +0300

    Project Update

--- home/views.py
+++ home/views.py
@@ -1,13 +1,16 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactForm, ContactFormMessage
+from product.models import Product
 
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page':'home'}
+    sliderdata = Product.objects.all()[:4]
+    context = {'setting': setting, 'page':'home', 'sliderdata':sliderdata}
     return render(request, 'index.html', context)
 
 def about(request):
@@ -21,6 +24,30 @@ def references(request):
     return render(request, 'references.html', context)
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            #*BEGIN
+
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                data.ip = x_forwarded_for.split(',')[0]
+            else:
+                data.ip = request.META.get('REMOTE_ADDR')
+            #*END
+           ## data.ip = request.META.get('REMOTE_ADDR')
+            data.save()  # save data to table
+            messages.success(request, "Your message has ben sent. Thank you for your message.")
+            return HttpResponseRedirect('/contact')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
-    return render(request, 'contact.html', context)
+    form = ContactForm()
+    context={'setting':setting,'form':form}
+    return render(request, 'contact.html',context)
+
+
