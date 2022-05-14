82c2e224727198a9d3d970e0211fad64868c90e5
cankayaci <can.kayaci01@gmail.com>
2022-05-14 17:52:43 +0300

    Project Update; User Order Product List and Detail

--- user/views.py
+++ user/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from order.models import Order, OrderProduct
 from product.models import Category
 from user.forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 from user.models import UserProfile
@@ -87,7 +88,8 @@ def user_update(request):
     else:
         category = Category.objects.all()
         user_form = UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.userprofile)  # "userprofile" model -> OneToOneField relatinon with user
+        profile_form = ProfileUpdateForm(
+            instance=request.user.userprofile)  # "userprofile" model -> OneToOneField relatinon with user
         context = {'category': category, 'user_form': user_form, 'profile_form': profile_form}
         return render(request, 'user_update.html', context)
 
@@ -102,9 +104,47 @@ def user_password(request):
             messages.success(request, 'Your password was successfully updated!')
             return HttpResponseRedirect('/user')
         else:
-            messages.error(request, 'Please correct the error below.<br>'+ str(form.errors))
+            messages.error(request, 'Please correct the error below.<br>' + str(form.errors))
             return HttpResponseRedirect('/user/password')
     else:
         category = Category.objects.all()
         form = PasswordChangeForm(request.user)
         return render(request, 'user_password.html', {'form': form, 'category': category})
+
+
+@login_required(login_url='/login')
+def user_orders(request):
+    category = Category.objects.all()
+    current_user = request.user
+    orders = Order.objects.filter(user_id=current_user.id)
+    context = {'category': category, 'orders': orders}
+    return render(request, 'user_orders.html', context)
+
+
+@login_required(login_url='/login')  # Check login
+def user_orderdetail(request, id):
+    category = Category.objects.all()
+    current_user = request.user
+    order = Order.objects.get(user_id=current_user.id, id=id)
+    orderitems = OrderProduct.objects.filter(order_id=id)
+    context = {'category': category, 'order': order, 'orderitems': orderitems}
+    return render(request, 'user_orderdetail.html', context)
+
+
+@login_required(login_url='/login')
+def user_order_product(request):
+    category = Category.objects.all()
+    current_user = request.user
+    order_product = OrderProduct.objects.filter(user_id=current_user.id).order_by('-id')
+    context = {'category': category, 'order_product': order_product}
+    return render(request, 'user_orderproducts.html', context)
+
+
+@login_required(login_url='/login')
+def user_order_product_detail(request, id, oid):
+    category = Category.objects.all()
+    current_user = request.user
+    order = Order.objects.get(user_id=current_user.id, id=oid)
+    orderitems = OrderProduct.objects.filter(id=id, user_id=current_user.id)
+    context = {'category': category, 'order': order, 'orderitems': orderitems}
+    return render(request, 'user_orderdetail.html', context)
